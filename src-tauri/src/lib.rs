0b51921bcf67c4ce8003977f5f65100564beb6e6
Stefano Novelli <murdercode@gmail.com>
2026-05-07 23:02:16 +0200

    fix: tighten tokio features, idempotent tracing init, keyring backends

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -9,12 +9,12 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tracing_subscriber::fmt()
+    let _ = tracing_subscriber::fmt()
         .with_env_filter(
             tracing_subscriber::EnvFilter::try_from_default_env()
                 .unwrap_or_else(|_| "info".into()),
         )
-        .init();
+        .try_init();
 
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
